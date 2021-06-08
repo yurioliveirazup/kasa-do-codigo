73f1ff8787b79677ffdb92177ca5512fbb38a936
Yuri Matheus <yurimatheusb@hotmail.com>
2021-06-08 14:22:15 -0300

    setup

---
+++ src/main/kotlin/br/com/zup/edu/kasadocodigo/KasaDoCodigoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.edu.kasadocodigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KasaDoCodigoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KasaDoCodigoApplication>(*args)
+}
